9b97737345d3279185052ddf9e1533b284d1d945
dimitris <triantafd@iti.gr>
2021-08-27 16:32:32 +0300

    hm

---
+++ part1/anecdotes/src/components/Button.js
@@ -0,0 +1,13 @@
+
+import { setToValue } from '../utils'
+
+
+const Button = (props) => {
+  return (
+    <button onClick={setToValue(props.func, props.value)}>
+      {props.name}
+    </button>
+  )
+}
+
+export default Button
