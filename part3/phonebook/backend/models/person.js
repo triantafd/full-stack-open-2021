87f4e3127b5431de92ee2835fcf349b0ffa82d63
dimitris <triantafd@iti.gr>
2021-09-02 02:28:08 +0300

    g

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator');
 
@@ -39,46 +38,4 @@ personSchema.set('toJSON', {
   }
 })
 
-=======
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
-
-/* mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);  */
-
-const url = process.env.MONGODB_URI
-console.log(url)
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message)
-  })
-
-const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 3
-  },
-  number: {
-    type: String,
-    required: true,
-    minlength: 8
-  }
-})
-
-personSchema.plugin(uniqueValidator)
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
->>>>>>> 565fac15ee7cc948115a8aa5125c3a33a7f54ddc
 module.exports = mongoose.model('Person', personSchema)
